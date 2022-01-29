0794f2e421e3a1ecbb654a1b06e644425af7fe86
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-29 02:36:57 +0000

    上传代码

---
+++ ForTutorial/sample1.lua
@@ -0,0 +1,19 @@
+print("count from 1 to 3")
+for i = 1, 3 do
+    print(i)
+end
+
+print("count from 3 to 1 in steps of 1. zero iterations!")
+for i = 3, 1 do
+    print(i)
+end
+
+print("count down from 3 to 1")
+for i = 3, 1, -1 do
+    print(i)
+end
+
+print("we're not limited to integers")
+for i = 1, 0, -0.25 do
+    print(i)
+end
